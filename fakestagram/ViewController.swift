7dd809d43418e5351c24749fd0d724bda0b7c1bb
yisslu <lugohalo999@gmail.com>
2024-09-27 16:19:27 -0600

    Initial Commit

---
+++ fakestagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fakestagram
+//
+//  Created by Diplomado on 27/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
